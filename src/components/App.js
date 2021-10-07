1b1ffa35ceefa24d1149bd96cfe02fadc4c3c8ff
dianeyasminec <dianeyasminec60@gmail.com>
2021-10-07 15:56:24 -0400

    assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
@@ -11,9 +11,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city}/>
+      <Home username={user.name} city={user.city} color={user.color}/>
       <About bio={user.bio} linkedin={user.links.linkedin} github={user.links.github} />
-      <Links github={user.links.github} linkedin={user.links.linkedin} />
+     
     </div>
   );
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -8,8 +8,8 @@ function Links(props){
     return(
 <div>
     <h3>Links</h3>
-    <a href={github}>github</a>
-    <a href={linkedin}>linkedin</a>
+    <a href={github}>{github}</a>
+    <a href={linkedin}>{linkedin}</a>
 
 </div>
     )}
